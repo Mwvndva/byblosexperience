29d1af2de6b857c2888e8b14c44a928cd569ac4d
Mwvndva <111277645+Mwvndva@users.noreply.github.com>
2025-07-23 02:05:57 +0300

    fixed migrations

--- server/migrations/add_password_reset_fields_to_organizers.sql
+++
@@ -1,4 +0,0 @@
--- Add password reset token and expiration fields to organizers table
-ALTER TABLE organizers 
-ADD COLUMN password_reset_token VARCHAR(255),
-ADD COLUMN password_reset_expires TIMESTAMP;
